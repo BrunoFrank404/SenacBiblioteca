9f1c1ddc51c8df9b1625881ba6161de740cd9d2b
BrunoFrank404 <frankbruno754_2@hotmail.com>
2025-08-29 14:53:53 -0300

    first commit

---
+++ src/main/java/model/Livro.java
@@ -0,0 +1,47 @@
+package model;
+
+public class Livro {
+    private int id;
+    private String titulo;
+    private String autor;
+    private Status status;
+
+    public enum Status {
+        DISPONIVEL, EMPRESTADO
+    }
+
+    public Livro(int id, String titulo, String autor) {
+        this.id = id;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.status = Status.DISPONIVEL;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+}
---
+++ src/test/java/org/senac/senacbiblioteca/SenacBibliotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.senac.senacbiblioteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SenacBibliotecaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
